140c2b9d860977fb86945604f5fc15e03c6aeff5
Fresh <s100757@ap.be>
2019-02-14 14:13:33 +0100

    Auto stash before merge of "Development" and "origin/Development"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule} from '@angular/router'
 import { AppComponent } from './app.component';
 import { StoriesGurselComponent } from './stories-gursel/stories-gursel.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StoriesGurselComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      //{path:"home",}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
