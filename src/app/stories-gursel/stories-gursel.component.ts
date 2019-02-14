7bbe6f14c282343d537da703c18324bfd9758f31
Gürsel Gökce <s101083@ap.be>
2019-02-14 14:09:09 +0100

    Component Gursel Gevoegd

---
+++ src/app/stories-gursel/stories-gursel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stories-gursel',
+  templateUrl: './stories-gursel.component.html',
+  styleUrls: ['./stories-gursel.component.css']
+})
+export class StoriesGurselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
