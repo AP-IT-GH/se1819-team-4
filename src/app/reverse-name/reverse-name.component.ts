339b290dba3b0b3dc041ef8b99d915cba152c96c
Amir <amirh.abrar@yahoo.com>
2019-02-18 14:17:31 +0100

    Update reverse name

--- src/app/reverse-name/reverse-name.component.ts
+++ src/app/reverse-name/reverse-name.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-reverse-name',
   templateUrl: './reverse-name.component.html',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReverseNameComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+}
+@Component({
+  selector: 'app-click-me',
+  template: `
+    <button (click)="onClickMe()">Click to reverse!</button>
+    {{clickMessage}}`
+})
+export class ClickMeComponent {
+  clickMessage = '';
+
+  onClickMe() {
+    this.clickMessage = 'You are my hero!';
+  }
 }
